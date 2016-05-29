ed772dde902f26b0652738caf1d0b949dec2adec
Geronimo-GG <d.i.beliaeva@yandex.ua>
2016-05-29 21:22:51 +0300

    xml new (activity_hello_name, xml_hello_advice, xml_hello_password)

--- app/src/main/java/dariabeliaeva/diploma/com/finefin/NewMain.java
+++ app/src/main/java/dariabeliaeva/diploma/com/finefin/NewMain.java
@@ -145,8 +145,6 @@ public class NewMain extends AppCompatActivity
             transaction.addToBackStack(null);
             transaction.commit();
 
-        } else if (id == R.id.nav_send) {
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
